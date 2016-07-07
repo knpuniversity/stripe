194908338df7f9da09c095e34c8a05122826062a
Ryan Weaver <ryan@thatsquality.com>
2016-07-07 15:29:39 -0400

    Adding Product entity with list/show pages and fixtures

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,11 +11,14 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function homepageAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $products = $this->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->findAll();
+
+        return $this->render('default/homepage.html.twig', array(
+            'products' => $products,
+        ));
     }
 }
