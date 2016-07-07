e6f683da92da17ff55915034bbaef9160d345ca9
Ryan Weaver <ryan@thatsquality.com>
2016-07-07 16:02:02 -0400

    Adding basic shopping cart and start to a checkout page

---
+++ src/AppBundle/Controller/OrderController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class OrderController extends Controller
+{
+    /**
+     * @Route("/cart/product/{slug}", name="order_add_product_to_cart")
+     * @Method("POST")
+     */
+    public function addProductToCartAction(Product $product)
+    {
+        $this->get('shopping_cart')
+            ->addProduct($product);
+
+        $this->addFlash('success', 'Product added!');
+
+        return $this->redirectToRoute('order_checkout');
+    }
+
+    /**
+     * @Route("/checkout", name="order_checkout")
+     */
+    public function checkoutAction()
+    {
+        $products = $this->get('shopping_cart')->getProducts();
+
+        return $this->render('order/checkout.html.twig', array(
+            'products' => $products,
+            'cart' => $this->get('shopping_cart')
+        ));
+
+    }
+}
